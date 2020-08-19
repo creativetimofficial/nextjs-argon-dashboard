861d408583ee3d391b76be1aa1d38ce7bdd181fa
EINazare <emanuelioannazare@gmail.com>
2020-08-19 18:21:01 +0300

    chore: try to fix the import of nucleo.css

--- next.config.js
+++ next.config.js
@@ -2,10 +2,11 @@ const withPlugins = require("next-compose-plugins");
 const withImages = require("next-images");
 const withSass = require("@zeit/next-sass");
 const withCSS = require("@zeit/next-css");
+const withFonts = require('next-fonts');
 const webpack = require("webpack");
 const path = require("path");
 
-module.exports = withPlugins([[withSass], [withImages], [withCSS]], {
+module.exports = withPlugins([[withSass], [withImages], [withCSS], [withFonts]], {
   webpack(config, options) {
     config.resolve.modules.push(path.resolve("./"));
     return config;
