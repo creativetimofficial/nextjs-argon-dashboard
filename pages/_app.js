2b2d49b0439b1b81a15c1ccb53d5a79c931978a8
EINazare <emanuelioannazare@gmail.com>
2020-08-19 17:59:58 +0300

    feature: add missing pages

---
+++ pages/_app.js
@@ -0,0 +1,23 @@
+// import App from 'next/app'
+
+import "assets/plugins/nucleo/css/nucleo.css";
+import "@fortawesome/fontawesome-free/css/all.min.css";
+import "assets/scss/argon-dashboard-react.scss";
+
+function MyApp({ Component, pageProps }) {
+  return <Component {...pageProps} />
+}
+
+// Only uncomment this method if you have blocking data requirements for
+// every single page in your application. This disables the ability to
+// perform automatic static optimization, causing every page in your app to
+// be server-side rendered.
+//
+// MyApp.getInitialProps = async (appContext) => {
+//   // calls page's `getInitialProps` and fills `appProps.pageProps`
+//   const appProps = await App.getInitialProps(appContext);
+//
+//   return { ...appProps }
+// }
+
+export default MyApp
---
+++ pages/_document.js
@@ -0,0 +1,33 @@
+import React from "react";
+import Document, { Head, Main, NextScript } from "next/document";
+
+class MyDocument extends Document {
+  render() {
+    return (
+      <html lang="en">
+        <Head>
+          <meta charSet="utf-8" />
+          <meta name="theme-color" content="#000000" />
+          <link rel="shortcut icon" href={require("assets/img/favicon.png")} />
+          <link
+            rel="apple-touch-icon"
+            sizes="76x76"
+            href={require("assets/img/apple-icon.png")}
+          />
+          {/* Fonts and icons */}
+          <link
+            href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700"
+            rel="stylesheet"
+          />
+        <title>NextJS Argon Dashboard by Creative Tim</title>
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </html>
+    );
+  }
+}
+
+export default MyDocument;
