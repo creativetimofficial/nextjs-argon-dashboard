ff5f0e2fee995c62f80be61a975ee2aa40f963f6
EINazare <emanuelioannazare@gmail.com>
2020-08-20 16:53:58 +0300

    refactor: make header dark

--- components/Headers/Header.js
+++ components/Headers/Header.js
@@ -24,7 +24,7 @@ class Header extends React.Component {
   render() {
     return (
       <>
-        <div className="header bg-gradient-info pb-8 pt-5 pt-md-8">
+        <div className="header bg-gradient-dark pb-8 pt-5 pt-md-8">
           <Container fluid>
             <div className="header-body">
               {/* Card stats */}
