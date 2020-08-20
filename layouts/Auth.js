e7a007060d041e0d4329b5505eed872af3c8f074
EINazare <emanuelioannazare@gmail.com>
2020-08-20 16:44:32 +0300

    fix: solve errors from Auth layout + made hooks

--- layouts/Auth.js
+++ layouts/Auth.js
@@ -25,57 +25,56 @@ import AuthFooter from "components/Footers/AuthFooter.js";
 
 import routes from "routes.js";
 
-class Auth extends React.Component {
-  componentDidMount() {
+function Auth(props) {
+  React.useEffect(() => {
     document.body.classList.add("bg-default");
-  }
-  componentWillUnmount() {
-    document.body.classList.remove("bg-default");
-  }
-  render() {
-    return (
-      <>
-        <div className="main-content">
-          <AuthNavbar />
-          <div className="header bg-gradient-info py-7 py-lg-8">
-            <Container>
-              <div className="header-body text-center mb-7">
-                <Row className="justify-content-center">
-                  <Col lg="5" md="6">
-                    <h1 className="text-white">Welcome!</h1>
-                    <p className="text-lead text-light">
-                      Use these awesome forms to login or create new account in
-                      your project for free.
-                    </p>
-                  </Col>
-                </Row>
-              </div>
-            </Container>
-            <div className="separator separator-bottom separator-skew zindex-100">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                preserveAspectRatio="none"
-                version="1.1"
-                viewBox="0 0 2560 100"
-                x="0"
-                y="0"
-              >
-                <polygon
-                  className="fill-default"
-                  points="2560 0 2560 100 0 100"
-                />
-              </svg>
+    // Specify how to clean up after this effect:
+    return function cleanup() {
+      document.body.classList.remove("bg-default");
+    };
+  }, []);
+  return (
+    <>
+      <div className="main-content">
+        <AuthNavbar />
+        <div className="header bg-gradient-info py-7 py-lg-8">
+          <Container>
+            <div className="header-body text-center mb-7">
+              <Row className="justify-content-center">
+                <Col lg="5" md="6">
+                  <h1 className="text-white">Welcome!</h1>
+                  <p className="text-lead text-light">
+                    Use these awesome forms to login or create new account in
+                    your project for free.
+                  </p>
+                </Col>
+              </Row>
             </div>
-          </div>
-          {/* Page content */}
-          <Container className="mt--8 pb-5">
-            <Row className="justify-content-center">{props.children}</Row>
           </Container>
+          <div className="separator separator-bottom separator-skew zindex-100">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              preserveAspectRatio="none"
+              version="1.1"
+              viewBox="0 0 2560 100"
+              x="0"
+              y="0"
+            >
+              <polygon
+                className="fill-default"
+                points="2560 0 2560 100 0 100"
+              />
+            </svg>
+          </div>
         </div>
-        <AuthFooter />
-      </>
-    );
-  }
+        {/* Page content */}
+        <Container className="mt--8 pb-5">
+          <Row className="justify-content-center">{props.children}</Row>
+        </Container>
+      </div>
+      <AuthFooter />
+    </>
+  );
 }
 
 export default Auth;
