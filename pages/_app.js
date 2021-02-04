6eed49c19cc749184115f297bf6177661df15c10
EINazare <emanuelioannazare@gmail.com>
2021-02-04 12:18:30 +0200

    fix: solve new maps

--- pages/_app.js
+++ pages/_app.js
@@ -70,6 +70,7 @@ export default class MyApp extends App {
             content="width=device-width, initial-scale=1, shrink-to-fit=no"
           />
           <title>NextJS Argon Dashboard by Creative Tim</title>
+          <script src="https://maps.googleapis.com/maps/api/js?key=YOUR_KEY_HERE"></script>
         </Head>
         <Layout>
           <Component {...pageProps} />
