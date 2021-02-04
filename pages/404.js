c4865275662025484f546883f31dd58bac11a0a4
EINazare <emanuelioannazare@gmail.com>
2021-02-04 12:56:45 +0200

    fix: solve export default issues

--- pages/404.js
+++ pages/404.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 import Router from "next/router";
 
-export function Error404() {
+export default function Error404() {
   React.useEffect(() => {
     Router.push("/admin/dashboard");
   });
