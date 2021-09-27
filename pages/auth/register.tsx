c75026fe01e6e9b27ec469b96d0882abe28f819c
nahsooyeon <nahsooyeon@gmail.com>
2021-09-27 09:40:19 +0000

    ♻️ 수정: tsx 로 변환

--- pages/auth/register.tsx
+++ pages/auth/register.tsx
@@ -16,20 +16,20 @@ import {
   Col,
 } from "reactstrap";
 // layout for this page
-import Auth from "layouts/Auth.js";
+import Auth from "../../layouts/Auth.js";
 
-function Login() {
+function Register() {
   return (
     <>
-      <Col lg="5" md="7">
+      <Col lg="6" md="8">
         <Card className="bg-secondary shadow border-0">
           <CardHeader className="bg-transparent pb-5">
-            <div className="text-muted text-center mt-2 mb-3">
-              <small>Sign in with</small>
+            <div className="text-muted text-center mt-2 mb-4">
+              <small>Sign up with</small>
             </div>
-            <div className="btn-wrapper text-center">
+            <div className="text-center">
               <Button
-                className="btn-neutral btn-icon"
+                className="btn-neutral btn-icon mr-4"
                 color="default"
                 href="#pablo"
                 onClick={(e) => e.preventDefault()}
@@ -60,11 +60,21 @@ function Login() {
           </CardHeader>
           <CardBody className="px-lg-5 py-lg-5">
             <div className="text-center text-muted mb-4">
-              <small>Or sign in with credentials</small>
+              <small>Or sign up with credentials</small>
             </div>
             <Form role="form">
-              <FormGroup className="mb-3">
-                <InputGroup className="input-group-alternative">
+              <FormGroup>
+                <InputGroup className="input-group-alternative mb-3">
+                  <InputGroupAddon addonType="prepend">
+                    <InputGroupText>
+                      <i className="ni ni-hat-3" />
+                    </InputGroupText>
+                  </InputGroupAddon>
+                  <Input placeholder="Name" type="text" />
+                </InputGroup>
+              </FormGroup>
+              <FormGroup>
+                <InputGroup className="input-group-alternative mb-3">
                   <InputGroupAddon addonType="prepend">
                     <InputGroupText>
                       <i className="ni ni-email-83" />
@@ -91,52 +101,47 @@ function Login() {
                   />
                 </InputGroup>
               </FormGroup>
-              <div className="custom-control custom-control-alternative custom-checkbox">
-                <input
-                  className="custom-control-input"
-                  id=" customCheckLogin"
-                  type="checkbox"
-                />
-                <label
-                  className="custom-control-label"
-                  htmlFor=" customCheckLogin"
-                >
-                  <span className="text-muted">Remember me</span>
-                </label>
+              <div className="text-muted font-italic">
+                <small>
+                  password strength:{" "}
+                  <span className="text-success font-weight-700">strong</span>
+                </small>
               </div>
+              <Row className="my-4">
+                <Col xs="12">
+                  <div className="custom-control custom-control-alternative custom-checkbox">
+                    <input
+                      className="custom-control-input"
+                      id="customCheckRegister"
+                      type="checkbox"
+                    />
+                    <label
+                      className="custom-control-label"
+                      htmlFor="customCheckRegister"
+                    >
+                      <span className="text-muted">
+                        I agree with the{" "}
+                        <a href="#pablo" onClick={(e) => e.preventDefault()}>
+                          Privacy Policy
+                        </a>
+                      </span>
+                    </label>
+                  </div>
+                </Col>
+              </Row>
               <div className="text-center">
-                <Button className="my-4" color="primary" type="button">
-                  Sign in
+                <Button className="mt-4" color="primary" type="button">
+                  Create account
                 </Button>
               </div>
             </Form>
           </CardBody>
         </Card>
-        <Row className="mt-3">
-          <Col xs="6">
-            <a
-              className="text-light"
-              href="#pablo"
-              onClick={(e) => e.preventDefault()}
-            >
-              <small>Forgot password?</small>
-            </a>
-          </Col>
-          <Col className="text-right" xs="6">
-            <a
-              className="text-light"
-              href="#pablo"
-              onClick={(e) => e.preventDefault()}
-            >
-              <small>Create new account</small>
-            </a>
-          </Col>
-        </Row>
       </Col>
     </>
   );
 }
 
-Login.layout = Auth;
+Register.layout = Auth;
 
-export default Login;
+export default Register;
--- variables/charts.js
+++ variables/charts.js
@@ -6,23 +6,7 @@ if (typeof document === "undefined") {
     querySelector: () => {},
   };
 }
-/*!
 
-=========================================================
-* NextJS Argon Dashboard - v1.1.0
-=========================================================
-
-* Product Page: https://www.creative-tim.com/product/nextjs-argon-dashboard
-* Copyright 2021 Creative Tim (https://www.creative-tim.com)
-* Licensed under MIT (https://github.com/creativetimofficial/nextjs-argon-dashboard/blob/master/LICENSE.md)
-
-* Coded by Creative Tim
-
-=========================================================
-
-* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
-
-*/
 const Chart = require("chart.js");
 //
 // Chart extension for making the bars rounded
